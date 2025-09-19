ef15b97582e49bcede0f4d0cd1ac25f82cef145f
clementaugustinowick <clement.augustinowick@epitech.eu>
2025-09-19 12:39:41 +0200

    [feat] build server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const http = require('http');
+const path = require('path');
 
+const port = process.env.PORT || 5000;
 const app = express();
 const server = http.createServer(app);
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../public')));
 
-server.listen(3000, () => {
-    console.log('Server running at http://localhost:3000');
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
 })
