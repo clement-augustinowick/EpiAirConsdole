1e57921ad237c5bb8195842582c9bf28a9ec1340
clementaugustinowick <clement.augustinowick@epitech.eu>
2025-10-05 13:28:34 +0200

    [feat]: create a slider for all games available in my EpiAirConsole

--- public/script.js
+++ public/script.js
@@ -1,6 +1,42 @@
 const socket = io('http://localhost:3000');
 
-let sessionID = null;
+
+const slidesData = [
+    {img: "images/comming-soon.jpg", text: "Comming soon"},
+    {img: "images/comming-soon.jpg", text: "Comming soon"},
+    {img: "images/comming-soon.jpg", text: "Comming soon"},
+    {img: "images/comming-soon.jpg", text: "Comming soon"},
+    {img: "images/comming-soon.jpg", text: "Comming soon"},
+    {img: "images/comming-soon.jpg", text: "Comming soon"},
+    {img: "images/comming-soon.jpg", text: "Comming soon"},
+    {img: "images/comming-soon.jpg", text: "Comming soon"},
+    {img: "images/comming-soon.jpg", text: "Comming soon"},
+    {img: "images/comming-soon.jpg", text: "Comming soon"}
+]
+
+const slider = document.querySelector(".slider-1 .slider");
+
+function createSlide({img, text}){
+    const slide = document.createElement("div");
+    slide.classList.add("slide");
+
+    const image = document.createElement("img");
+    image.src = img;
+    image.alt = text;
+
+    const description = document.createElement("p");
+    description.classList.add("description");
+    description.textContent = text;
+
+    slide.appendChild(image);
+    slide.appendChild(description);
+
+    return slide;
+}
+
+[...slidesData, ...slidesData].forEach((slideData) => {
+    slider.appendChild(createSlide(slideData));
+});
 
 // MAIN SCREEN -> SERVER
 document.getElementById('menu-btn-start-session').addEventListener('click', () => {
