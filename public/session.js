235b75fd87df9cff7c247b43e54476210beee04d
clementaugustinowick <clement.augustinowick@epitech.eu>
2025-10-09 10:42:07 +0200

    [feat]: add pop-up with help link for player who can't scan the QR code

--- public/session.js
+++ public/session.js
@@ -1,13 +1,28 @@
 const socket = io('http://localhost:3000');
 
+const popup = document.getElementById('pop-up-help-qrcode');
+
+document.getElementById('qrcode-help-1').addEventListener('click', () => {
+    popup.style.display = 'flex';
+});
+
+document.getElementById('qrcode-help-2').addEventListener('click', () => {
+    popup.style.display = 'flex';
+});
+
+document.getElementById('close-pop-up').addEventListener('click', () => {
+    popup.style.display = 'none';
+});
+
 
 // MAIN SCREEN -> SERVER
 document.getElementById('btn-generate-session').addEventListener('click', () => {
     socket.emit('create-session', null, (response) => {
         document.getElementById('session-code').textContent = `Session code : ${response.sessionID}`;
         document.getElementById('QRCode-container').innerHTML = `<img src="${response.qrDataUrl}" alt="QR code to connect players to session">`;
-        // document.getElementById('session-link').textContent = `Lien de la page pour smartphone : ${response.link}`;
+        document.getElementById('session-link').textContent = response.link;
         document.querySelectorAll('.qrcode-help').forEach(btn => {btn.style.display = 'block'});
+        document.getElementById('player-display').style.display = 'flex';
     });
 });
 
